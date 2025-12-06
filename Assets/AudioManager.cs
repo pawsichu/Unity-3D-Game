346a93ac7a96560034391547c1d8bd530e774526
Luke <artxpoyn@gmail.com>
2025-12-06 18:49:53 +0100

    Mainmenu

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    public AudioClip background;
+    public AudioClip jumpSFX;
+    public AudioClip walkSFX;
+    public AudioClip deathSFX;
+    public AudioClip interactSFX;
+    public AudioClip buttonSFX;
+
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+}
+
+

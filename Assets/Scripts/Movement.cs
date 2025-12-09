bec08c0381046f4e5020383cfca740fe3657cc02
Luke <artxpoyn@gmail.com>
2025-12-09 07:59:37 +0100

    music, scenes
    .

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,46 +2,51 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed;
+    public float speed, jumpForce;
     public float groundDist;
 
+    private Vector2 moveInput;
+
     public LayerMask groundLayer;
+    public Transform groundCheck;
+    private bool isGrounded;
+
     public Rigidbody rb;
     public SpriteRenderer sr;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
-        Vector3 castPos = transform.position;
-        castPos.y += 0.1f; // Slightly raise the raycast position to avoid self-collision
-        if (Physics.Raycast(castPos, -transform.up, out hit, Mathf.Infinity, groundLayer))
-        {
-            if (hit.collider != null)
-            {
-                Vector3 movePos = transform.position;
-                movePos.y = hit.point.y + groundDist; // Set the y position to be above the ground by groundDist
-                transform.position = movePos;
-            }
+        // Check if the player is grounded
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDist, groundLayer);
 
+        // Handle jumping
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Debug.Log("Jumped");
+            Vector3 newVelocity = rb.linearVelocity;
+            newVelocity.y = jumpForce; // Set the upward velocity for the jump
+            rb.linearVelocity = newVelocity;
         }
 
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
-        Vector3 moveDir = new Vector3(x, 0, y);
-        rb.linearVelocity = moveDir * speed;
+        // Handle movement
+        moveInput.x = Input.GetAxis("Horizontal");
+        moveInput.y = Input.GetAxis("Vertical");
+        Vector3 moveDir = new Vector3(moveInput.x, 0, moveInput.y);
 
-        if (x != 0 && x < 0)
+        // Preserve the y velocity while updating horizontal movement
+        rb.linearVelocity = new Vector3(moveDir.x * speed, rb.linearVelocity.y, moveDir.z * speed);
+
+        // Flip the sprite based on movement direction
+        if (moveInput.x != 0 && moveInput.x < 0)
         {
             sr.flipX = true;
         }
-        else if (x != 0 && x > 0)
+        else if (moveInput.x != 0 && moveInput.x > 0)
         {
             sr.flipX = false;
         }
--- Assets/Scripts/SceneManage.cs
+++ Assets/Scripts/SceneManage.cs
@@ -5,6 +5,16 @@ using UnityEngine.SceneManagement;
 public class SceneManage : MonoBehaviour
 {
     public string sceneName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the object colliding is the player
+        if (other.CompareTag("Player"))
+        {
+            changeScene();
+        }
+    }
+
     public void changeScene()
     {
         SceneManager.LoadScene(sceneName);
